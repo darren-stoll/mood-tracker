400e5ba3abd79e34f84aff6b9c8835eb7e728784
darren-stoll <darrenstoll11@gmail.com>
2022-02-23 10:47:13 -0700

    Added more pages. Applied global css. Footer added

---
+++ components/footer.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+const Footer = () => {
+  return (
+    <Link href="../">
+      Back to main
+    </Link>
+  )
+}
+
+export default Footer
---
+++ pages/moodEntry.tsx
@@ -0,0 +1,15 @@
+import styles from '../styles/MoodEntry.module.css'
+import Footer from '../components/footer'
+
+const MoodEntry = () => {
+  return (
+    <div className="container">
+      <main className="main">
+        This is for a new mood entry.
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default MoodEntry
