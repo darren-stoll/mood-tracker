1449b23c755bf9c0db17de01608131643dcb9672
darren-stoll <darrenstoll11@gmail.com>
2022-02-24 14:16:25 -0700

    Developed first part of mood entry page. Still needs functionality.

--- pages/moodEntry.tsx
+++ pages/moodEntry.tsx
@@ -1,11 +1,34 @@
 import styles from '../styles/MoodEntry.module.css'
 import { Footer } from '../components/Footer'
+import { useState } from 'react'
+import Link from 'next/link'
 
 const MoodEntry = () => {
+  // State that holds selected mood
+  const [mood, setMood] = useState('');
+  const [comment, setComment] = useState('');
+
   return (
     <div className="container">
       <main className="main">
-        This is for a new mood entry.
+        <div className={styles.entry}>
+          <div className={styles.emojis}>
+            <div onClick={() => setMood('overjoyed')}>😃</div>
+            <div onClick={() => setMood('confident')}>😎</div>
+            <div onClick={() => setMood('apathetic')}>😐</div>
+            <div onClick={() => setMood('despairing')}>☹️</div>
+            <div onClick={() => setMood('angry')}>😠</div>
+          </div>
+          <div className={styles.commentBox}>
+            <textarea placeholder="Reason/Cause" onChange={e => setComment(e.target.value)}></textarea>
+          </div>
+          {/* Have it link to a confirmation window, which redirects to history after 5 seconds */}
+          <div className={styles.submit}>
+            <Link href="/">
+              <a className={styles.submitAnchor}>Submit</a>
+            </Link>
+          </div>
+        </div>
       </main>
       <Footer />
     </div>
