2c5a915b86e27f33ea4e76aec8f6d65bc03289de
darren-stoll <darrenstoll11@gmail.com>
2022-03-07 10:29:16 -0700

    Added styling for Footer and MoodHistory entries.

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export const Footer = () => {
   return (
     <Link href="../">
-      <a>Back to main</a>
+      <a style={{paddingBottom:"50px"}}>Back to main</a>
     </Link>
   )
 }
