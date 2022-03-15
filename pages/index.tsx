721c7a2447d2f2279bede26157ce064db19d03eb
darren-stoll <darrenstoll11@gmail.com>
2022-03-15 13:02:41 -0600

    Added another API + handling.

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,12 +14,25 @@ const Home: NextPage = () => {
 
   // Generate an image from one of several animal APIs randomly selected
   useEffect(() => {
-    axios.get('https://dog.ceo/api/breeds/image/random')
-      .then((response) => {
-        setImage(response.data.message)
-        setLoading(false)
-      });
-    }, []);
+    const ANIMALAPICOUNT = 2;
+    const PICKANAPI = Math.ceil(Math.random() * ANIMALAPICOUNT);
+
+    if (PICKANAPI === 1) {
+      axios.get('https://dog.ceo/api/breeds/image/random')
+        .then((response) => {
+          setImage(response.data.message)
+          setLoading(false)
+        });
+    }
+    else if (PICKANAPI === 2) {
+      axios.get('https://api.thecatapi.com/v1/images/search')
+        .then((response) => {
+          setImage(response.data[0].url)
+          setLoading(false)
+        });
+    }
+  }, []);
+    
 
   if (loading) {
     return (
